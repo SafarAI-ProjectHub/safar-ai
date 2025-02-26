270c5de1970cd708bc264b89423aeddded64e589
Mays Alkhalil <maysalkhalil02@gmail.com>
2025-02-26 16:08:03 +0300

    Enhanced units management and UI improvements
    Refactored the unit management system by improving UI interactions, fixing modal scrolling issues, and enhancing AJAX request handling. Updated the DataTable initialization to support dynamic filters and added better error handling for failed API requests. Improved the Canvas drawing tool by fixing object layering issues and optimizing event listeners for smoother interactions.

--- resources/views/dashboard/admin/courses.blade.php
+++ resources/views/dashboard/admin/courses.blade.php
@@ -34,7 +34,7 @@
                             <th>Level</th>
                             <th>Type</th>
                             <th>Teacher</th>
-                            <th>Block</th> 
+                            <th>Block</th>
                             <th>Completed</th>
                             <th>Actions</th>
                         </tr>
@@ -46,117 +46,113 @@
 
     @can('create courses')
     <!-- Add Course Modal -->
-            <div class="modal fade" id="addCourseModal" tabindex="-1" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <form id="addCourseForm" enctype="multipart/form-data">
-                            @csrf
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="addCourseModalLabel">Add New Unit</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-
-                                <div class="mb-3">
-                                    <label for="title" class="form-label">Unit Title</label>
-                                    <input type="text" class="form-control" id="title" name="title" required>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="description" class="form-label">Description</label>
-                                    <textarea class="form-control" id="description" name="description" required></textarea>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="category_id" class="form-label">Category</label>
-                                    <select class="form-select" id="category_id" name="category_id" required>
-                                        <option value="" disabled selected>Select Category</option>
-                                        @foreach ($categories as $category)
-                                            <option value="{{ $category->id }}">{{ $category->age_group }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="level" class="form-label">Level</label>
-                                    <select class="form-select" id="level" name="level" required>
-                                        <option value="" disabled selected>Select Level</option>
-                                        @for ($i = 1; $i <= 6; $i++)
-                                            <option value="{{ $i }}">Level {{ $i }}</option>
-                                        @endfor
-                                    </select>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="type" class="form-label">Type</label>
-                                    <select class="form-select" id="type" name="type" required>
-                                        <option value="" disabled selected>Select Type</option>
-                                        <option value="weekly">Weekly</option>
-                                        <option value="intensive">Intensive</option>
-                                    </select>
-                                </div>
-
-                                <div class="mb-3">
-                                <label for="block_name" class="form-label">Block</label>
-                                <label for="block_id" class="form-label">Block</label>
-                                <select class="form-select" id="block_id" name="block_id" required>
-                                    <option value="" disabled selected>Select Block</option>
-                                    @foreach($blocks as $block)
-                                        <option value="{{ $block->id }}">{{ $block->name }}</option>
-                                    @endforeach
-                                </select>
-
-
-
-
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="image" class="form-label">Upload Image</label>
-                                    <input type="file" class="filepond" name="image" data-allow-reorder="true"
-                                        data-max-file-size="5MB" data-max-files="1">
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Add Unit</button>
-                            </div>
-                        </form>
+    <div class="modal fade" id="addCourseModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <form id="addCourseForm" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addCourseModalLabel">Add New Unit</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                </div>
-            </div> 
- @endcan
+                    <div class="modal-body">
 
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Unit Title</label>
+                            <input type="text" class="form-control" id="title" name="title" required>
+                        </div>
 
-    @hasanyrole('Super Admin|Admin')
-        <!-- Assign Teacher Modal -->
-        <div class="modal fade" id="assignTeacherModal" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-                <div class="modal-content">
-                    <form id="assignTeacherForm">
-                        @csrf
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="assignTeacherModalLabel">Assign Teacher to Unit</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea class="form-control" id="description" name="description" required></textarea>
                         </div>
-                        <div class="modal-body">
-                            <input type="hidden" id="assignCourseId" name="course_id">
-                            <div class="mb-3">
-                                <label for="teacher_id" class="form-label">Select Teacher</label>
-                                <select class="form-select" id="teacher_id" name="teacher_id" required style="width: 100%;">
-                                    <option value="" disabled selected>Select Teacher</option>
-                                </select>
-                            </div>
+
+                        <div class="mb-3">
+                            <label for="category_id" class="form-label">Category</label>
+                            <select class="form-select" id="category_id" name="category_id" required>
+                                <option value="" disabled selected>Select Category</option>
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->age_group }}</option>
+                                @endforeach
+                            </select>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Assign Teacher</button>
+
+                        <div class="mb-3">
+                            <label for="level" class="form-label">Level</label>
+                            <select class="form-select" id="level" name="level" required>
+                                <option value="" disabled selected>Select Level</option>
+                                @for ($i = 1; $i <= 6; $i++)
+                                    <option value="{{ $i }}">Level {{ $i }}</option>
+                                @endfor
+                            </select>
                         </div>
-                    </form>
-                </div>
+
+                        <div class="mb-3">
+                            <label for="type" class="form-label">Type</label>
+                            <select class="form-select" id="type" name="type" required>
+                                <option value="" disabled selected>Select Type</option>
+                                <option value="weekly">Weekly</option>
+                                <option value="intensive">Intensive</option>
+                            </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="block_name" class="form-label">Block</label>
+                            <label for="block_id" class="form-label">Block</label>
+                            <select class="form-select" id="block_id" name="block_id" required>
+                                <option value="" disabled selected>Select Block</option>
+                                @foreach($blocks as $block)
+                                    <option value="{{ $block->id }}">{{ $block->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Upload Image</label>
+                            <input type="file" class="filepond" name="image" data-allow-reorder="true"
+                                   data-max-file-size="5MB" data-max-files="1">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Add Unit</button>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
+    @endcan
+
+    @hasanyrole('Super Admin|Admin')
+    <!-- Assign Teacher Modal -->
+    <div class="modal fade" id="assignTeacherModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <form id="assignTeacherForm">
+                    @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="assignTeacherModalLabel">Assign Teacher to Unit</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="assignCourseId" name="course_id">
+                        <div class="mb-3">
+                            <label for="teacher_id" class="form-label">Select Teacher</label>
+                            <select class="form-select" id="teacher_id" name="teacher_id" required style="width: 100%;">
+                                <option value="" disabled selected>Select Teacher</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Assign Teacher</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     @endhasanyrole
+
     <input type="hidden" id="delete_source" value="{{ route('admin.courses.delete') }}">
 @endsection
 
@@ -209,7 +205,7 @@
                     defaultContent: 'N/A'
                 },
                 {
-                    data: 'block_name', 
+                    data: 'block_name',
                     name: 'block_name',
                     defaultContent: 'No Block'
                 },
@@ -220,8 +216,8 @@
                     searchable: false,
                     render: function(data, type, row) {
                         return `<div class="form-check form-switch">
-                                <input class="form-check-input toggle-complete" type="checkbox" data-id="${row.id}" ${data ? 'checked' : ''}>
-                            </div>`;
+                                    <input class="form-check-input toggle-complete" type="checkbox" data-id="${row.id}" ${data ? 'checked' : ''}>
+                                </div>`;
                     }
                 },
                 {
@@ -231,7 +227,6 @@
                     searchable: false
                 }
             ],
-
             columnDefs: [{
                 visible: false,
                 targets: 0
@@ -299,20 +294,20 @@
         // Submit form for adding course/unit
         $('#addCourseForm').on('submit', function(e) {
             e.preventDefault();
-            var blockValue = $('#block_id').val(); 
-            console.log('Selected Block:', blockValue); 
+            var blockValue = $('#block_id').val();
+            console.log('Selected Block:', blockValue);
 
             var originalFormData = new FormData(this);
             var newFormData = new FormData();
 
-            // Iterate over each entry in the original form data
+            // نسخ البيانات باستثناء 'image' لإضافتها يدويًا
             for (var pair of originalFormData.entries()) {
                 if (pair[0] !== 'image') {
                     newFormData.append(pair[0], pair[1]);
                 }
             }
 
-            // Add the image file separately
+            // إضافة ملف الصورة من FilePond إذا وُجد
             var file = pond.getFile();
             if (file) {
                 newFormData.append('image', file.file);
@@ -329,14 +324,14 @@
                     table.ajax.reload();
                     showAlert('success', 'Unit added successfully!', 'bxs-check-circle');
 
-                    // Clear input fields
+                    // تفريغ الحقول
                     $('#title').val('');
                     $('#description').val('');
                     $('#category_id').val('');
                     $('#level').val('');
                     $('#type').val('');
 
-                    // Clear FilePond
+                    // إزالة الملفات من FilePond
                     pond.removeFiles();
                 },
                 error: function(response) {
@@ -369,7 +364,7 @@
                         allowClear: true
                     });
 
-                    // Pre-select the assigned teacher if available
+                    // إذا أردت اختيار مُدرس افتراضيًا:
                     var selectedTeacherId = $('#assignCourseId').data('teacher-id');
                     if (selectedTeacherId) {
                         $('#teacher_id').val(selectedTeacherId).trigger('change');
@@ -402,16 +397,13 @@
             });
         });
 
-        // Clear modal on close
+        // عند إغلاق المودال يتم تفريغ الحقول
         $('#addCourseModal').on('hidden.bs.modal', function() {
-            // Clear input fields
             $('#title').val('');
             $('#description').val('');
             $('#category_id').val('');
             $('#level').val('');
             $('#type').val('');
-
-            // Clear FilePond
             pond.removeFiles();
         });
 
@@ -489,8 +481,6 @@
                 }
             });
         });
-
     });
-</script>
-
+    </script>
 @endsection
